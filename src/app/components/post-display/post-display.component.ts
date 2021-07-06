18041b95f554bbb69455bc3c9d120cf91eed8b56
Ecko95 <joshuaduffill@gmail.com>
2021-07-06 20:10:45 +0200

    added manual http request to load posts with async pipe

--- src/app/components/post-display/post-display.component.ts
+++ src/app/components/post-display/post-display.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 // Model
-import { PostModel } from "../../models/post.model";
-import { UserModel } from "../../models/user.model";
+import { PostModel } from '../../models/post.model';
+import { UserModel } from '../../models/user.model';
 
 // Service
-import { PostsService } from "../../services/posts.service";
-import { UsersService } from "../../services/users.service";
+import { PostsService } from '../../services/posts.service';
+import { UsersService } from '../../services/users.service';
 
 @Component({
-  selector: "app-post-display",
-  templateUrl: "./post-display.component.html",
-  styleUrls: ["./post-display.component.css"]
+  selector: 'app-post-display',
+  templateUrl: './post-display.component.html',
+  styleUrls: ['./post-display.component.css']
 })
 export class PostDisplayComponent implements OnInit {
   posts: PostModel[];
   users: UserModel[];
+  postsToLoad: Observable<any>;
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
 
   constructor(
     private postsService: PostsService,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private http: HttpClient
   ) {
     this.getPosts();
   }
@@ -37,6 +42,13 @@ export class PostDisplayComponent implements OnInit {
     console.log(this.posts);
   }
 
+  getPostsAsync() {
+    // let params = new HttpParams().set('id', '1');
+    // let headers = new HttpHeaders().set('Authorization', 'auth-token');
+
+    this.postsToLoad = this.http.get(this.ROOT_URL + '/posts');
+  }
+
   setUserName() {
     if (this.posts && this.users) {
       for (const post of this.posts) {
@@ -49,5 +61,7 @@ export class PostDisplayComponent implements OnInit {
     }
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getPostsAsync();
+  }
 }
